c569e5482988eae67e849889749309abb66f145f
Pasidu <152088727+Hackerpasindu@users.noreply.github.com>
2023-11-27 23:00:41 +0530

    Add files via upload

---
+++ ban.sh
@@ -0,0 +1,36 @@
+echo
+echo -e $g "██       ██╗           ██████╗██╗         ██╗██████╗  ██████╗    █████"
+echo -e $g "██║   ██╔╝        ██╔════╝██║        ██║██╔══██╗██╔══██╗██╔══██╗"
+echo -e $g "█████═╝        ╚█████╗     ██║          ██║██║      ██║██║    ██║███████║"
+echo -e $g "██╔═██╗              ╚═══██╗██║        ██║██║     ██║██║      ██║██╔══██║"
+echo -e $g "██║   ╚██╗██╗██████╔╝╚██████╔╝██████╔╝██████╔╝██║     ██║"
+echo -e $g "╚═╝      ╚═╝╚═╝╚═════╝     ╚═════╝  ╚═════╝    ╚═════╝   ╚═╝     ╚═╝"
+echo -e $r "                                                                       tool by kingsudda"
+echo -e $y "what is your king Banner name ? : \c"
+read kingsudda
+echo
+echo -e $y "what is your king name ? : \c"
+read name
+echo
+echo "cowsay -f eyes "$name" | lolcat" > name.txt
+echo "figlet "$name" | lolcat" > kingsudda.txt
+echo "clear" > clear.txt
+echo "PSI-\s" > Temp.txt
+echo
+rm -rf /data/data/com.termux/files/usr/etc/zshrc
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+echo
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "kingsudda.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+echo
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "kingsudda.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo
+rm -rf name.txt kingsudda.txt clear.txt Temp.txt
+echo
+figlet "finish kingsudda" | lolcat
+echo -e $r "please Restart your termux"
